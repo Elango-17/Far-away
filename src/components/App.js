e8a05336c7613fdb81d6335c8ff0157c6c158988
Elangovan R <123308498+Elango17@users.noreply.github.com>
2024-12-31 15:30:53 +0530

    Initial Comit

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Logo } from "./logo";
+import { Form } from "./Form";
+import { PackingList } from "./PackingList";
+import { Stats } from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDelete(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClear() {
+    const confirm = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirm) setItems([]);
+  }
+
+  return (
+    <div className="App">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDelete}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClear}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
